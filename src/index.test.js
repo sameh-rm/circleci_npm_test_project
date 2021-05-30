73b7f6eab3baf523cb20752278876670c0c92548
Sameh Ramadan <sameh.r.sayed@gmail.com>
2021-05-30 12:37:47 +0200

    first commit

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+const { sum } = require("./index.js");
+const n = (a) => null;
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("should return null", () => {
+  expect(n()).toBe(null);
+});
